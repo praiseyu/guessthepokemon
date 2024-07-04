c71b9dae080008d139ebd73d7489d1d2c438d10e
Praise Yu <praiseyu@gmail.com>
2024-07-03 22:12:34 -0600

    add favicon and clean up main code

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 import PokemonGame from "./pokemonGame-api.js";
-
 const pokemonGame = new PokemonGame();
 const imgContainer = document.querySelector(".pokemon__container");
 const image = document.createElement("img");
@@ -12,112 +11,94 @@ const resetButton = document.querySelector(".guess-form__reset");
 let attempts = 0;
 let pokemonName;
 
-// make Pokemon gets a new pokemon from api
 const makePokemon = async () => {
-    image.removeAttribute("class");
-    let pokemon = await pokemonGame.getPokemon();
-    const src = pokemon.img;
-    const pokemonName = pokemon.name;
-    image.classList.add("pokemon__image");
-    image.setAttribute("src", src);
-    imgContainer.appendChild(image);
-    return pokemonName;
+  image.removeAttribute("class");
+  let pokemon = await pokemonGame.getPokemon();
+  const src = pokemon.img;
+  const pokemonName = pokemon.name;
+  image.classList.add("pokemon__image");
+  image.setAttribute("src", src);
+  imgContainer.appendChild(image);
+  return pokemonName;
 };
 
 pokemonName = await makePokemon();
-console.log(pokemonName);
-
 guessForm.addEventListener("submit", onSubmitGuess);
 
-function onSubmitGuess(e){
-    e.preventDefault();
-    feedback.innerText="";
-    if(e.target.guess.value.trim() === ""){
-        input.classList.add("guess-form__input--invalid");
-        feedback.innerText="Enter a name please.";
-    }
-    else{
-        const guess = e.target.guess.value;
-        input.classList.remove("guess-form__input--invalid");
-        console.log(guess);
-        guessForm.reset();
-        evaluateGuess(guess, pokemonName, feedback);
-    }
+function onSubmitGuess(e) {
+  e.preventDefault();
+  feedback.innerText = "";
+  if (e.target.guess.value.trim() === "") {
+    input.classList.add("guess-form__input--invalid");
+    feedback.innerText = "Enter a name please.";
+  }
+  else {
+    const guess = e.target.guess.value;
+    input.classList.remove("guess-form__input--invalid");
+    guessForm.reset();
+    evaluateGuess(guess, pokemonName, feedback);
+  }
 
 }
 
 function evaluateGuess(guess, pokemonName, feedback) {
-    if (checkGuess(guess, pokemonName)) {
-    //   console.log("correct");
-      image.classList.add("pokemon__image--original");
-      
-      if(attempts>=1){
-        feedback.classList.remove("pokemon__message--incorrect");
-      }
-      feedback.classList.add("pokemon__message--correct");
-      feedback.innerText = "Congratulations you are a master Pokémon poacher ";
-  
-      resetButton.classList.add("guess-form__reset--newgame");
-
-      displayName();
-      resetButton.addEventListener("click", addNewGame);
-      enterGuessText.classList.add("guess-form__text--hide");
-
-    } else {
-      attempts++;
-  
-      feedback.innerText = `You guessed "${guess}": that is incorrect! Two more tries.`;
-      feedback.classList.add("pokemon__message--incorrect");
-      switch (attempts) {
-        case 1:
-          image.classList.add("pokemon__image--second");
-          console.log("Try again first fail");
-          break;
-        case 2:
-          image.classList.add("pokemon__image--third");
-          feedback.innerText = `You guessed "${guess}": that is incorrect. One more try left.`;
-          console.log("Try again second fail");
-          break;
-        default:
-          image.classList.add("pokemon__image--original");
-          feedback.classList.add("pokemon__message--correct");
-          feedback.innerText = `The correct answer is "${pokemonName}"! New Game?`;
-          displayName();
-          resetButton.classList.add("guess-form__reset--newgame");
-          resetButton.addEventListener("click", addNewGame);
-          enterGuessText.classList.add("guess-form__text--hide");
-          break;
-      }
+  if (checkGuess(guess, pokemonName)) {
+    image.classList.add("pokemon__image--original");
+    if (attempts >= 1) {
+      feedback.classList.remove("pokemon__message--incorrect");
+    }
+    feedback.classList.add("pokemon__message--correct");
+    feedback.innerText = "Congratulations you are a master Pokémon poacher ";
+    resetButton.classList.add("guess-form__reset--newgame");
+    displayName();
+    resetButton.addEventListener("click", addNewGame);
+    enterGuessText.classList.add("guess-form__text--hide");
+  } else {
+    attempts++;
+    feedback.innerText = `You guessed "${guess}". That is incorrect! Two more tries.`;
+    feedback.classList.add("pokemon__message--incorrect");
+    switch (attempts) {
+      case 1:
+        image.classList.add("pokemon__image--second");
+        break;
+      case 2:
+        image.classList.add("pokemon__image--third");
+        feedback.innerText = `You guessed "${guess}": that is incorrect. One more try left.`;
+        break;
+      default:
+        image.classList.add("pokemon__image--original");
+        feedback.classList.add("pokemon__message--correct");
+        feedback.innerText = `The correct answer is "${pokemonName}"! New Game?`;
+        displayName();
+        resetButton.classList.add("guess-form__reset--newgame");
+        resetButton.addEventListener("click", addNewGame);
+        enterGuessText.classList.add("guess-form__text--hide");
+        break;
     }
   }
+}
 
-async function addNewGame(e){
-    e.preventDefault();
-    pokemonName = await makePokemon();
-    attempts = 0;
-
-    title.classList.remove("pokemon__name--show");
-    enterGuessText.classList.remove("guess-form__text--hide");
-
-    feedback.innerText="";
-    feedback.classList.remove("pokemon__message--correct");
-    feedback.classList.remove("pokemon__message--incorrect");
-    
-    resetButton.classList.remove("guess-form__reset--newgame");
+async function addNewGame(e) {
+  e.preventDefault();
+  pokemonName = await makePokemon();
+  attempts = 0;
+  title.classList.remove("pokemon__name--show");
+  enterGuessText.classList.remove("guess-form__text--hide");
+  feedback.innerText = "";
+  feedback.classList.remove("pokemon__message--correct");
+  feedback.classList.remove("pokemon__message--incorrect");
+  resetButton.classList.remove("guess-form__reset--newgame");
 }
 
 // HELPER FUNCTIONS
-
-
 function checkGuess(name, pokemon) {
-    if (name == pokemon) {
-      return true;
-    }
-    return false;
+  if (name == pokemon) {
+    return true;
   }
-
+  return false;
+}
 
 function displayName() {
-    title.innerText = pokemonName;
-    title.classList.add("pokemon__name--show");
+  title.innerText = pokemonName;
+  title.classList.add("pokemon__name--show");
 }
--- js/pokemonGame-api.js
+++ js/pokemonGame-api.js
@@ -1,15 +1,13 @@
 class PokemonGame {
-    constructor() {
-      this.baseUrl = `https://pokeapi.co/api/v2/pokemon/`; //id will be a randon number between 1-500
-    }
-    async getPokemon() {
-      const id = Math.floor(Math.random() * (500 - 1) + 1);
-      const baseUrl = `${this.baseUrl}${id}/`; //id will be a randon number between 1-500
-      const res = await axios.get(baseUrl);
-      // console.log(res.data.species.name);
-      return { img: res.data.sprites.front_shiny, name: res.data.species.name };
-    }
+  constructor() {
+    this.baseUrl = `https://pokeapi.co/api/v2/pokemon/`;
   }
-  
-  export default PokemonGame;
-  
+  async getPokemon() {
+    const id = Math.floor(Math.random() * (500 - 1) + 1);
+    const baseUrl = `${this.baseUrl}${id}/`;
+    const res = await axios.get(baseUrl);
+    return { img: res.data.sprites.front_shiny, name: res.data.species.name };
+  }
+}
+
+export default PokemonGame;
